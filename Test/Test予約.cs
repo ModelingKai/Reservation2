0987e2d32ca61ed692e13a32b67f773ef246cca7
Jun Nakajima <ikuta1919@gmail.com>
2020-10-05 00:16:06 +0900

    [RED] 30日より先だと予約ができない

--- Test/Test予約.cs
+++ Test/Test予約.cs
@@ -16,16 +16,25 @@ namespace Test
             // TODO:DateTime.Nowはかなり怪しい。けど、頭がふやふやなので、いまはこれでいく。
             Assert.Throws<ArgumentException>(() =>  new 利用期間(かいし,しゅうりょう, DateTime.Now));
         }
-        // [Fact]
-        // public void 利用期間が19時より後を含んでいると予約ができない()
-        // {
-        //     
-        // }
-        // [Fact]
-        // public void 利用期間が30日より先だと予約ができない()
-        // {
-        //     
-        // }
-        //
+        [Fact]
+        public void 利用期間が19時より後を含んでいると予約ができない()
+        {
+            var かいし = new 開始年月日時分(2020, 10, 20, 10, 0);
+            var しゅうりょう = new 終了年月日時分(2020, 10, 20, 19,15);
+            
+            // TODO:DateTime.Nowはかなり怪しい。けど、頭がふやふやなので、いまはこれでいく。
+            Assert.Throws<ArgumentException>(() =>  new 利用期間(かいし,しゅうりょう, DateTime.Now));
+            
+        }
+        [Fact]
+        public void 利用期間が30日より先だと予約ができない()
+        {
+            var かいし = new 開始年月日時分(2021, 10, 20, 10, 0);
+            var しゅうりょう = new 終了年月日時分(2021, 10, 20, 10,15);
+            
+            // TODO:DateTime.Nowはかなり怪しい。けど、頭がふやふやなので、いまはこれでいく。
+            Assert.Throws<ArgumentException>(() =>  new 利用期間(かいし,しゅうりょう, DateTime.Now));
+        }
+        
     }
 }
