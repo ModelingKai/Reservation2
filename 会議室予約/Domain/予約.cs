a3edd3dd6fa9fbdb938532426ef5abc3c02ecde6
Jun Nakajima <ikuta1919@gmail.com>
2020-10-04 22:00:20 +0900

    予約期間　→　利用期間　に命名変更
    あくまで会議室を利用する期間なので。

--- 会議室予約/Domain/予約.cs
+++ 会議室予約/Domain/予約.cs
@@ -7,14 +7,14 @@ namespace 会議室予約.Domain
     {
         // どんなモノもっている？
         private 予約者 よやくしゃ;
-        private 予約期間_むらさき よやくきかん;
+        private 利用期間 りようきかん;
         private 会議室 かいぎしつ;
         private 会議参加予定者 かいぎさんかよていしゃ;
     }
 
-    internal class 予約期間_むらさき
+    internal class 利用期間
     {
-        private 利用開始年月日時分 りようかいしねんがっぴ;
-        private 利用終了年月日時分 りようしゅうねんがっぴ;
+        private 開始年月日時分 かいしねんがっぴ;
+        private 終了年月日時分 しゅうねんがっぴ;
     }
 }
