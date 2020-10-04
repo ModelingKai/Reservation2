fbe2397313122221b81acbbda9e037792db648d9
Jun Nakajima <ikuta1919@gmail.com>
2020-10-04 22:15:15 +0900

    予約可能ルールを型として追加

--- 会議室予約/Domain/予約.cs
+++ 会議室予約/Domain/予約.cs
@@ -5,11 +5,29 @@ namespace 会議室予約.Domain
     /// </summary>
     public class 予約
     {
-        // どんなモノもっている？
         private 予約者 よやくしゃ;
         private 利用期間 りようきかん;
         private 会議室 かいぎしつ;
         private 会議参加予定者 かいぎさんかよていしゃ;
+        
+        
+        // 操作・メソッド考える？
+        // public 予約()
+        // {
+        //     if(予約可能ルール().isOK())
+        // }
+    }
+
+    /// <summary>
+    /// 30日以内なら予約可能なやつ
+    /// </summary>
+    internal class 予約可能ルール
+    {
+        public bool IsOk() => true;
+    }
+    
+    internal class 予約者
+    {
     }
 
     internal class 利用期間
@@ -18,3 +36,4 @@ namespace 会議室予約.Domain
         private 終了年月日時分 しゅうねんがっぴ;
     }
 }
+
