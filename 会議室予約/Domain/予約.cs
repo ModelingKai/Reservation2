29922775ed7f2c97774889de5a88265975ab3655
Jun Nakajima <ikuta1919@gmail.com>
2020-10-04 21:40:01 +0900

    予約集約の雛形を作った

---
+++ 会議室予約/Domain/予約.cs
@@ -0,0 +1,22 @@
+namespace 会議室予約.Domain
+{
+    /// <summary>
+    /// 予約ルートエンティティ
+    /// </summary>
+    public class 予約
+    {
+        // どんなモノもっている？
+        private 予約日 よやくび;
+        private 予約者 よやくしゃ;
+        private 予約期間_むらさき よやくきかん;
+        private 会議参加予定者 かいぎさんかよていしゃ;
+        private 会議室 かいぎしつ;
+        // TODO: これは会議参加予定者から算出できそう
+        private 利用人数_むらさき りようにんずう;
+    }
+
+    internal class 予約期間_むらさき
+    {
+      
+    }
+}
