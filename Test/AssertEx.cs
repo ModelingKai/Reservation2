a1f425d36c0491f605fda85635f6e08ba9b3dfde
t2-kob <4531723+t2-kob@users.noreply.github.com>
2020-10-18 19:57:44 +0900

    Test用の拡張メソッドを作成。

---
+++ Test/AssertEx.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Test
+{
+    public static class AssertEx
+    {
+        public static void Fail()
+        {
+            true.Is(false);
+        }
+    }
+}
