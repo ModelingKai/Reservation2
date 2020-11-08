3a55b3ad7d688ca2b700a59a76114340074706d2
t2-kob <4531723+t2-kob@users.noreply.github.com>
2020-11-08 22:16:51 +0900

    Add 予約希望リポジトリのInterfaceを作った。

---
+++ 会議室予約/UseCase/Repository/I予約Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using 会議室予約.Domain;
+
+namespace 会議室予約.UseCase.Repository
+{
+    public class I予約Repository
+    {
+        internal Task Add(予約 よやく)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- 会議室予約/UseCase/疑似ユースケースクラス.cs
+++ 会議室予約/UseCase/疑似ユースケースクラス.cs
@@ -2,15 +2,20 @@ using System.Threading.Tasks;
 using 会議室予約.Domain;
 using 会議室予約.Domain.Exceptions;
 using 会議室予約.UseCase.Exceptions;
+using 会議室予約.UseCase.Repository;
 
 namespace 会議室予約.UseCase
 {
     public class 疑似ユースケースクラス
     {
-        public class 疑似ユースケースクラス()
+        private readonly I予約Repository _repository;
+
+        public 疑似ユースケースクラス(I予約Repository repository)
         {
-            
+            _repository = repository;
         }
+
+
         
         public async Task 会議室予約するAsync(予約Request request)
         {
@@ -22,7 +27,7 @@ namespace 会議室予約.UseCase
                     request.かいぎさんかよていしゃ,
                     new 予約可能ルール());
                             
-                await repository.save(よやく);
+                await _repository.Add(よやく);
             }
             catch (ルール違反Exception ex)
             {  
